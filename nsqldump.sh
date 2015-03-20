85014fdf01d19fb4c16ef95e2b8864491a87799e
Nonoo <nonoo@nonoo.hu>
2015-03-20 15:20:11 +0100

    init

---
+++ nsqldump.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+self=`readlink "$0"`
+if [ -z "$self" ]; then
+	self=$0
+fi
+scriptname=`basename "$self"`
+scriptdir=${self%$scriptname}
+
+. $scriptdir/config
+. $nlogrotatepath/redirectlog.src.sh
+
+if [ "$1" = "quiet" ]; then
+	quietmode=1
+	redirectlog
+fi
+
+checklogsize
+
+mysqldump -u $mysqluser -p$mysqlpass --all-databases | gzip -9 > $dstpath/`hostname`-sqldump-`date +%Y%m%d`.sql.gz
+find $dstpath/* -mtime +$deleteafterdays -exec rm {} \;
