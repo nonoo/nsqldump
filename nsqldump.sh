e302ab4f370315ce79f480015c662af2ff2b3081
Nonoo <nonoo@nonoo.hu>
2015-03-23 13:02:21 +0100

    added error mail sending

--- nsqldump.sh
+++ nsqldump.sh
@@ -17,4 +17,7 @@ fi
 checklogsize
 
 mysqldump -u $mysqluser -p$mysqlpass --all-databases | gzip -9 > $dstpath/`hostname`-sqldump-`date +%Y%m%d`.sql.gz
+if [ $? -ne 0 ]; then
+    eval $senderrormailcommand
+fi
 find $dstpath/* -mtime +$deleteafterdays -exec rm {} \;
